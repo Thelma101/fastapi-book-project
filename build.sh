ed5b042137352c8485258c37647687901a9d5322
Thelma101 <akpata.thelma@gmail.com>
2025-02-12 23:32:09 +0100

    Added build script for Nginx installation

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
-# Install Nginx using Nix
-nix-env -iA nixpkgs.nginx
+# #!/bin/bash
+# # Install Nginx using Nix
+# nix-env -iA nixpkgs.nginx
 
-# Copy the Nginx configuration file
-cp nginx.conf /etc/nginx/nginx.conf
+# # Copy the Nginx configuration file
+# cp nginx.conf /etc/nginx/nginx.conf
+
+#!/bin/bash
+
+# Install nginx
+apt-get update && apt-get install -y nginx
+
+# Start nginx
+nginx
+
+# Start FastAPI with Uvicorn
+uvicorn main:app --host 0.0.0.0 --port=${PORT}
